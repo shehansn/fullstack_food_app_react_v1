820cddaa70ac4bedb484f57521670e233853f5d9
10749822@students.plymouth.ac.uk <10749822@students.plymouth.ac.uk>
2023-05-31 23:32:39 +0530

    adding products and cart completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
 import { BrowserRouter as Router } from "react-router-dom";
-
 import App from "./App";
 import { StateProvider } from "./context/StateProvider";
-import { initialState } from "./context/initalState";
+import { initialState } from "./context/initialState";
 import reducer from "./context/reducer";
+
 
 ReactDOM.render(
     <Router>
